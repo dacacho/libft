0460ae3534654b24787d236c2309909055c93e2c
BakarSemper <daca.cacho@gmail.com>
2022-02-21 22:27:16 +0000

    Add files via upload

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: danierod <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 16:58:53 by danierod          #+#    #+#             */
-/*   Updated: 2022/02/21 17:14:28 by danierod         ###   ########.fr       */
+/*   Updated: 2022/02/21 22:22:22 by danierod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,22 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	char	*h;
-	char	*n;
+	size_t	h;
+	size_t	n;
 
-	h = (char *)haystack;
-	n = (char *)needle;
-	if (n == 0)
-		return (&*h);
-	while (--len)
+	if (*needle == '\0' || needle == NULL)
+		return ((char *)haystack);
+	h = 0;
+	while (haystack[h] != '\0' && h < len)
 	{
-		if (h == n)
-			return (&*h);
+		n = 0;
+		while (needle[n] == haystack[n + h] && h + n < len)
+		{
+			if (needle[n + 1] == '\0')
+				return ((char *)&haystack[h]);
+			n++;
+		}
 		h++;
-		n++;
 	}
 	return (NULL);
 }
@@ -35,7 +38,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 int	main()
 {
 	char	haystack[] = "hello world how we doing today?";
-	char	needle[] = "how";
+	char	needle[] = "doing";
 
 	printf("%s", ft_strnstr(haystack, needle, 50));
 	return (0);
